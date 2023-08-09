85653a93ba235d946e86ddd85972e15d9aad173a
Sardar sain <88813071+sardarsain67@users.noreply.github.com>
2023-08-09 07:34:47 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,14 +1,14 @@
 <?php
-$host = 'localhost';          // MySQL server hostname
-$username = 'root';  // MySQL username
-$password = '';  // MySQL password
-$database = 'event';  // MySQL database name
+$host = 'sql6.freesqldatabase.com';  // MySQL server hostname
+$username = 'sql6638506';            // MySQL username
+$password = 'xZsxtUZWuv';            // MySQL password
+$database = 'sql6638506';            // MySQL database name
+$port = 3306;                        // Port number
 
 // Create a new MySQLi object and establish the connection
-$conn = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database, $port);
 
 if (!$conn) {
     die('Could not connect to MySQL: ' . mysqli_connect_error());
 } 
-
 ?>
